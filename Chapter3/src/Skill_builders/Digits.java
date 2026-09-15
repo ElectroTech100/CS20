2b487e247714b9a95868382f0dff3c238a71a69a
510348001 <510348001@L815WD220020.edu.cbe.ab.ca>
2026-09-15 11:35:10 -0600

    11:35AM

--- Chapter3/src/Skill_builders/Digits.java
+++ Chapter3/src/Skill_builders/Digits.java
@@ -1,6 +1,34 @@
 package Skill_builders;
 
+import java.util.Scanner;
+
 public class Digits {
 
+	public static void main(String[] args) {
+		//Declaration
+		int ones;
+		int tens;
+		int num;
+		int faketen;
+				
+				
+		//Create Scanner object
+		Scanner userinput = new Scanner(System.in);
+				
+		//Get user input
+		System.out.print("Enter a 2 digit number:  ");
+		num = userinput.nextInt();
+				
+		// the math
+		tens = (num / 10) * 10;
+		faketen = num / 10;
+		ones = num - (faketen * 10);
+				
+		//Display the two-digit number in both the tens-place and the ones-place
+		System.out.println("The ones-place of " + num + " is: " + ones);
+		System.out.println("The tens-place of " + num + " is: " + faketen + " or " + tens);
+				
+
+	}
+
 }
-ldcmaszlkhlaskdlasjdlasd
