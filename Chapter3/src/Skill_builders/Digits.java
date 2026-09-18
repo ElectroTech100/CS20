cdc4730c78bad8562798205c103da2927bdf66e6
510348001 <510348001@L815WD220020.edu.cbe.ab.ca>
2026-09-18 09:08:09 -0600

    9:08AM

--- Chapter3/src/Skill_builders/Digits.java
+++ Chapter3/src/Skill_builders/Digits.java
@@ -8,26 +8,41 @@ public class Digits {
 		//Declaration
 		int ones;
 		int tens;
+		int Hund;
 		int num;
 		int tensplace;
-				
+		int hundsplace;		
 				
 		//Create Scanner object
 		Scanner userinput = new Scanner(System.in);
 				
 		//Get user input
-		System.out.print("Enter a 2 digit number:  ");
+		System.out.print("Enter a 3 digit number:  ");
 		num = userinput.nextInt();
 				
 		// the math
-		tens = (num / 10) * 10;
-		tensplace = num / 10;
-		ones = num - (tensplace * 10);
+		hundsplace = num / 100 ;
+		ones = num - ((num / 10 )* 10);
+		tensplace = (num - (hundsplace * 100) - ones) / 10;
+		Hund = hundsplace * 100;
+		tens = tensplace * 10;
+		
+		
 		
 		//Output
-	    System.out.println("The tens-place of " + num + " is: " + tensplace + " or " + tens);
+		System.out.println("The Hundreds-place of " + num + " is: " + hundsplace + " or " + Hund);
+		System.out.println("The tens-place of " + num + " is: " + tensplace + " or " + tens);
+	   
 	    System.out.println("The ones-place of " + num + " is: " + ones);
-				
+			
+	    
+	    /*
+	     *  Enter a 3 digit number:  325
+			The Hundreds-place of 325 is: 3 or 300
+			The tens-place of 325 is: 2 or 20
+			The ones-place of 325 is: 5
+
+	     */
 
 	}
 
