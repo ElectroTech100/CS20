43903cda36a6c10affbbd2709926fd99a94c6edd
510348001 <510348001@L815WD220020.edu.cbe.ab.ca>
2026-09-21 09:26:15 -0600

    9:26AM

---
+++ Chapter3/src/Mastery/Digits.java
@@ -0,0 +1,34 @@
+package Mastery;
+
+import java.util.Scanner;
+
+public class Digits {
+
+	public static void main(String[] args) {
+		//Declaration
+		int ones;
+		int tens;
+		int num;
+		int tensplace;
+				
+				
+		//Create Scanner object
+		Scanner userinput = new Scanner(System.in);
+				
+		//Get user input
+		System.out.print("Enter a 2 digit number:  ");
+		num = userinput.nextInt();
+				
+		// the math
+		tens = (num / 10) * 10;
+		tensplace = num / 10;
+		ones = num - (tensplace * 10);
+		
+		//Output
+	    System.out.println("The tens-place of " + num + " is: " + tensplace + " or " + tens);
+	    System.out.println("The ones-place of " + num + " is: " + ones);
+				
+
+	}
+
+}
